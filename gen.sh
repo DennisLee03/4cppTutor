470caa92f00d415bc9d80c6d63a7c2c5ba9dd49f
DennisLee03 <e64112287@gs.ncku.edu.tw>
2025-07-25 19:21:52 +0800

    Stop tracking files and update .gitignore

--- gen.sh
+++
@@ -1,8 +0,0 @@
-if [ -d "$1" ]; then
-    echo "✅ Directory '$1' exists." 
-    exit 0
-else 
-    cp -r template "$1"
-    echo "✅ Directory '$1' built."
-    exit 0
-fi
--- ttt-game/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-g++ main.cpp fun.cpp -o ttt -I./
-./bin/ttt
